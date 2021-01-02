c256dde37ba229704ef2b362d9cb08cd930614f2
anthos <guptaankur803@gmail.com>
2021-01-03 00:59:10 +0530

    Updates

---
+++ precache-manifest.17441b43fbab876d3cb4dba4395d618c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a12f01d0162ea574bd50f43582ca23",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ab0a6d9ff2900dfce40f",
+    "url": "/portfolio/static/css/2.82cf3036.chunk.css"
+  },
+  {
+    "revision": "9f86797946c3fc455db6",
+    "url": "/portfolio/static/css/main.893ef1e7.chunk.css"
+  },
+  {
+    "revision": "ab0a6d9ff2900dfce40f",
+    "url": "/portfolio/static/js/2.902e303c.chunk.js"
+  },
+  {
+    "revision": "ca2abe649ff858defa21eaf931086938",
+    "url": "/portfolio/static/js/2.902e303c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f86797946c3fc455db6",
+    "url": "/portfolio/static/js/main.13b4b4f5.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "0a5f3df629f86f11b157ff52823c05c7",
+    "url": "/portfolio/static/media/Amazon.0a5f3df6.gif"
+  },
+  {
+    "revision": "37f5fe9fb5ceec393fbf3d908e50b7c8",
+    "url": "/portfolio/static/media/Spotify.37f5fe9f.gif"
+  },
+  {
+    "revision": "1a0657576a4688c011efc1753685e808",
+    "url": "/portfolio/static/media/background.1a065757.jpeg"
+  },
+  {
+    "revision": "5351fd13890c190b7516d2b2614bf425",
+    "url": "/portfolio/static/media/csharp.5351fd13.svg"
+  },
+  {
+    "revision": "85fb4ea5eee9d547890e92ba66c9fee2",
+    "url": "/portfolio/static/media/css3.85fb4ea5.svg"
+  },
+  {
+    "revision": "9cea44c3a2b06a538570986c2e88883d",
+    "url": "/portfolio/static/media/docker.9cea44c3.svg"
+  },
+  {
+    "revision": "5cb92fedd54485318793a0e2331f84ee",
+    "url": "/portfolio/static/media/git.5cb92fed.svg"
+  },
+  {
+    "revision": "fc069f88865f12b57c27c6f93f08ddb2",
+    "url": "/portfolio/static/media/headshot.fc069f88.jpg"
+  },
+  {
+    "revision": "408f0dfb1f8bb16fe170ab80da2b80a6",
+    "url": "/portfolio/static/media/html5.408f0dfb.svg"
+  },
+  {
+    "revision": "f08be36753f15174415bbb94b441deb6",
+    "url": "/portfolio/static/media/hulu.f08be367.gif"
+  },
+  {
+    "revision": "4ad94aa82f48304b17acb8368b19c2d0",
+    "url": "/portfolio/static/media/javascript.4ad94aa8.svg"
+  },
+  {
+    "revision": "ac2a11043c48fde4772ddc68c1be9b3f",
+    "url": "/portfolio/static/media/landing.ac2a1104.jpg"
+  },
+  {
+    "revision": "84f743ac18ada9afcb207db15cd05643",
+    "url": "/portfolio/static/media/nodejs.84f743ac.svg"
+  },
+  {
+    "revision": "249f34e055d223a95a345a01defb2ee0",
+    "url": "/portfolio/static/media/project.249f34e0.svg"
+  },
+  {
+    "revision": "7f6f2a5268b7559bd6c44f5f43549351",
+    "url": "/portfolio/static/media/react.7f6f2a52.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
